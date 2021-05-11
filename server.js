84d928fa093a2d65c749e567a4aff2e55d64570b
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-05-11 15:31:00 -0400

    Deploy BMMT application

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-BMMT-front-end'));
+app.use(express.static('./dist/BMMT-front-end'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-BMMT-front-end/'}),
+  res.sendFile('index.html', {root: 'dist/BMMT-front-end/'}),
 );
 
 // Start the app by listening on the default Heroku port
