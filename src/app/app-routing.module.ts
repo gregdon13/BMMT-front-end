b00d216a1c551bdeb510cd476d5de9409ad311b6
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-05-12 16:53:05 -0400

    updates to the restructuring of the front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {HelpComponent} from './help/help.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {TransferComponent} from './transfer/transfer.component';
+import {CheckingAccountComponent} from './checking-account/checking-account.component';
+import {SavingsAccountComponent} from './savings-account/savings-account.component';
+import {InvestmentAccountComponent} from './investment-account/investment-account.component';
+import {DevPortalComponent} from './dev-portal/dev-portal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'help', component: HelpComponent},
+  { path: 'checking', component: CheckingAccountComponent},
+  { path: 'savings', component: SavingsAccountComponent},
+  { path: 'investment', component: InvestmentAccountComponent},
+  { path: 'Create-Profile', component: UserProfileComponent},
+  { path: 'transfer', component: TransferComponent },
+  { path: 'devPage', component: DevPortalComponent  },
+
+  // otherwise redirects to home
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/money-account.spec.ts
@@ -0,0 +1,7 @@
+import { MoneyAccount } from './money-account';
+
+describe('MoneyAccount', () => {
+  it('should create an instance', () => {
+    expect(new MoneyAccount()).toBeTruthy();
+  });
+});
