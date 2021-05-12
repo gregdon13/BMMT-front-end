2731e4debf871a68e0d1a01be49f4c68415ee93e
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-05-12 17:29:44 -0400

    more updates

--- src/app/models/money-account.ts
+++ src/app/models/money-account.ts
@@ -1,2 +1,64 @@
-export class MoneyAccount {
+interface Deserializable {
+  deserialize(input: any): this;
+}
+
+export class MoneyAccount implements Deserializable {
+  private _ID: number;
+  private _ACCOUNT_TYPE: string;
+  private _BALANCE: number;
+  private _ACCOUNT_NUMBER: number;
+  private _USER_ID: number;
+
+  // constructor(id: number, accountType: string, balance: number, accountNumber: number, userId: number) {
+  //   this._ID = id;
+  //   this._ACCOUNT_TYPE = accountType;
+  //   this._BALANCE = balance;
+  //   this._ACCOUNT_NUMBER = accountNumber;
+  //   this._USER_ID = userId;
+  // }
+
+
+  get ID(): number {
+    return this._ID;
+  }
+
+  set ID(value: number) {
+    this._ID = value;
+  }
+
+  get ACCOUNT_TYPE(): string {
+    return this._ACCOUNT_TYPE;
+  }
+
+  set ACCOUNT_TYPE(value: string) {
+    this._ACCOUNT_TYPE = value;
+  }
+
+  get BALANCE(): number {
+    return this._BALANCE;
+  }
+
+  set BALANCE(value: number) {
+    this._BALANCE = value;
+  }
+
+  get ACCOUNT_NUMBER(): number {
+    return this._ACCOUNT_NUMBER;
+  }
+
+  set ACCOUNT_NUMBER(value: number) {
+    this._ACCOUNT_NUMBER = value;
+  }
+
+  get USER_ID(): number {
+    return this._USER_ID;
+  }
+
+  set USER_ID(value: number) {
+    this._USER_ID = value;
+  }
+
+  deserialize(input: any): this {
+    return Object.assign(this, input);
+  }
 }
--- src/app/models/transaction.ts
+++ src/app/models/transaction.ts
@@ -1,2 +1,85 @@
-export class Transaction {
+interface Deserializable {
+  deserialize(input: any): this;
+}
+
+export class Transaction implements Deserializable{
+  private _ID: number;
+  private _TRANSACTION_DATE: Date;
+  private _ACCOUNT_ONE: number;
+  private _ACCOUNT_TWO: number | undefined;
+  private _TRANSACTION_TYPE: string;
+  private _AMOUNT: number;
+  private _USER_ID: number;
+
+  // constructor(id: number, transactionDate: Date, accountOne: number, transactionType: string,
+  //             amount: number, userId: number, accountTwo?: number) {
+  //   this._ID = id;
+  //   this._TRANSACTION_DATE = transactionDate;
+  //   this._ACCOUNT_ONE = accountOne;
+  //   this._ACCOUNT_TWO = accountTwo;
+  //   this._TRANSACTION_TYPE = transactionType;
+  //   this._AMOUNT = amount;
+  //   this._USER_ID = userId;
+  // }
+
+  deserialize(input: any): this {
+        return Object.assign(this, input);
+    }
+
+
+  get ID(): number {
+    return this._ID;
+  }
+
+  set ID(value: number) {
+    this._ID = value;
+  }
+
+  get TRANSACTION_DATE(): Date {
+    return this._TRANSACTION_DATE;
+  }
+
+  set TRANSACTION_DATE(value: Date) {
+    this._TRANSACTION_DATE = value;
+  }
+
+  get ACCOUNT_ONE(): number {
+    return this._ACCOUNT_ONE;
+  }
+
+  set ACCOUNT_ONE(value: number) {
+    this._ACCOUNT_ONE = value;
+  }
+
+  get ACCOUNT_TWO(): number | undefined {
+    return this._ACCOUNT_TWO;
+  }
+
+  set ACCOUNT_TWO(value: number | undefined) {
+    this._ACCOUNT_TWO = value;
+  }
+
+  get TRANSACTION_TYPE(): string {
+    return this._TRANSACTION_TYPE;
+  }
+
+  set TRANSACTION_TYPE(value: string) {
+    this._TRANSACTION_TYPE = value;
+  }
+
+  get AMOUNT(): number {
+    return this._AMOUNT;
+  }
+
+  set AMOUNT(value: number) {
+    this._AMOUNT = value;
+  }
+
+  get USER_ID(): number {
+    return this._USER_ID;
+  }
+
+  set USER_ID(value: number) {
+    this._USER_ID = value;
+  }
 }
